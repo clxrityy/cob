6a04dd70717c498684bf2d0d8e08eae8f8b3e0d7
clxrityy <97744702+clxrityy@users.noreply.github.com>
2023-05-22 16:21:30 -0400

    begin the journey

--- src/commands/birthday/birthday.ts
+++ src/commands/birthday/birthday.ts
@@ -1,12 +1,41 @@
+import { ApplicationCommandOptionType } from "discord.js";
 import { birthdayCmdEmbed } from "../../lib/data/birthdayCmds";
-import { fetchMember } from "../../lib/database/db";
+import { fetchMember, setBirthday } from "../../lib/database/db";
 import { Command } from "../../structures/Command";
 
 export default new Command({
-    name: 'birthday',
+    name: 'bd',
     description: 'All the birthday commands.',
+    options: [
+        {
+            name: 'set',
+            description: 'Set your birthday! (can only do it once)',
+            type: ApplicationCommandOptionType.String,
+            required: false,
+            autocomplete: true,
+        },
+        {
+            name: 'list',
+            description: 'List all the guild birthdays!',
+            type: ApplicationCommandOptionType.String,
+            required: false,
+            autocomplete: true,
+        },
+        {
+            name: 'user',
+            description: "See a specific user's birthday!",
+            type: ApplicationCommandOptionType.User,
+            required: false,
+        }
+
+    ],
     run: async ({ interaction }) => {
-        await interaction.followUp({ embeds: [birthdayCmdEmbed] });
-        await fetchMember(interaction.user.id, interaction.guildId);
+
+        const birthday = interaction.options.get('set').value as string;
+
+        // await interaction.followUp({ embeds: [birthdayCmdEmbed] });
+        // await fetchMember(interaction.user.id, interaction.guildId);
+        await interaction.followUp(`${birthday}`);
+        await setBirthday(interaction.user.id, interaction.guild.id, birthday);
     },
 })
